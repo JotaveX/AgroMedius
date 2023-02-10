3d8a3d8b367f97aec5c33b68cb64f2f6b5cdc100
Joao Victor Piloni <jvpiloni@gmail.com>
2023-02-10 09:36:24 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { MapaDePragasComponent } from './pages/mapa-de-pragas/mapa-de-pragas.component';
+import { AdicionarPragaComponent } from './pages/adicionar-praga/adicionar-praga.component';
+import { AddButtonComponent } from './pages/add-button/add-button.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    MapaDePragasComponent,
+    AdicionarPragaComponent,
+    AddButtonComponent
   ],
   imports: [
     BrowserModule,
+    GoogleMapsModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
